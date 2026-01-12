e25accbd36b3b8a74295ba92493c62be5bb87f12
Kate Lanina <lnnktrn@gmail.com>
2026-01-12 07:38:47 -0800

    Generated Spring Boot project structure.

---
+++ src/main/java/com/lnnktrn/timetravel_rainbow/TimetravelRainbowApplication.java
@@ -0,0 +1,13 @@
+package com.lnnktrn.timetravel_rainbow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimetravelRainbowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimetravelRainbowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lnnktrn/timetravel_rainbow/TimetravelRainbowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lnnktrn.timetravel_rainbow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimetravelRainbowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
